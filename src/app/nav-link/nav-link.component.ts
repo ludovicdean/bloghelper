711a6a1af21b46096b5ffd81736f6a4cd02af487
ludo <ludovicd58@gmail.com>
2025-03-26 23:39:52 +0100

    pages responsives et amélioration des outils

--- src/app/nav-link/nav-link.component.ts
+++ src/app/nav-link/nav-link.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -12,4 +12,9 @@ export class NavLinkComponent {
 @Input() route: string;
 @Input() text: string;
 @Input() isActive: boolean;
+@Output() linkClicked = new EventEmitter<void>();
+
+onLinkClicked() {
+  this.linkClicked.emit();
+}
 }
--- src/app/unsplash/unsplash.component.ts
+++ src/app/unsplash/unsplash.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthorComponent } from '../author/author.component';
 import { ImageConverterComponent } from "../image-converter/image-converter.component";
+import { CommonModule, NgClass, NgSwitch } from '@angular/common';
 
 @Component({
   selector: 'app-unsplash',
-  imports: [AuthorComponent, ImageConverterComponent],
+  imports: [AuthorComponent, ImageConverterComponent, NgClass, CommonModule, NgSwitch],
   templateUrl: './unsplash.component.html',
   styleUrl: './unsplash.component.css',
   standalone: true
 })
 export class UnsplashComponent {
+  activeTab: 'author' | 'imageConverter' = 'author';
 
+  setActiveTab(tab: 'author' | 'imageConverter') {
+    this.activeTab = tab;
+  }
 }
