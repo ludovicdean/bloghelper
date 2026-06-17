2ee6a8734b6e9d9d3ab15af32bf208c2394b62b3
Ludovic DÉAN <l.dean@facilit.bzh>
2026-06-17 23:35:21 +0200

    amélioration traitement des infos photos

--- src/app/unsplash/unsplash.component.ts
+++ src/app/unsplash/unsplash.component.ts
@@ -1,30 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthorComponent } from '../author/author.component';
 import { ImageConverterComponent } from "../image-converter/image-converter.component";
-import { CommonModule, NgClass, NgSwitch } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-unsplash',
-  imports: [AuthorComponent, ImageConverterComponent, NgClass, CommonModule, NgSwitch],
+  imports: [AuthorComponent, ImageConverterComponent, CommonModule],
   templateUrl: './unsplash.component.html',
   standalone: true
 })
-export class UnsplashComponent implements OnInit {
-  activeTab: 'author' | 'imageConverter' | null = null;  // ← null au début
-
-  ngOnInit() {
-    const savedTab = localStorage.getItem('activeTab') as 'author' | 'imageConverter';
-    if (savedTab) {
-      this.activeTab = savedTab;
-    } else {
-      this.activeTab = 'imageConverter';  // ← Directement ici
-    }
-    localStorage.setItem('activeTab', this.activeTab);  // Sauvegarde immédiate
-  }
-
-  setActiveTab(tab: 'author' | 'imageConverter') {
-    this.activeTab = tab;
-    localStorage.setItem('activeTab', tab);
-  }
+export class UnsplashComponent {
 }
 
